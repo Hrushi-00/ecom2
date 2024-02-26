29f1a96e81b161be37a0ded6a7459883ccd65164
Hrushi-00 <rushiskapse@gmail.com>
2024-02-26 10:38:16 +0530

    update information

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,6 +1,6 @@
 export const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
+
       breakpoint: { max: 4000, min: 1024 },
       items: 1
     },
@@ -29,23 +29,6 @@ export const responsive = {
         description:"A specialist label creating luxury essentials. Ethically crafted with an unwavering commitment to exceptional quality.",
         name:"Fall - Winter Collections 2030"
     },
-    // {
-    //     id:3,
-    //     image:"https://themewagon.github.io/malefashion/img/hero/hero-1.jpg",
-    //     price:"$200",
-    //     description:"A specialist label creating luxury essentials. Ethically crafted with an unwavering commitment to exceptional quality.",
-    //     name:"Fall - Winter Collections 2030"}
-    // {
-    //     id:4,
-    //     image:"https://themewagon.github.io/malefashion/img/hero/hero-1.jpg",
-    //     price:"$200",
-    //     description:"description",
-    // },
-    // {
-    //     id:5,
-    //     image:"https://themewagon.github.io/malefashion/img/hero/hero-2.jpg",
-    //     price:"$200",
-    //     description:"description",
-    // }
+    
   ]
 
